1c9cd4774a27d01d6b684b3710401703c35c6d71
unknown <ahnafakib@iut-dhaka.edu>
2023-03-13 02:56:40 +0600

    init

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+//creating root component
+const root = ReactDOM.createRoot(document.getElementById("root"))
+//render the root
+root.render(
+    <>
+    <App/>
+    </>
+)
